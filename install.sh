420e3a54217e25cc3f71ceb7917c8a2510d2270b
Curtis Bridges <curtis@curtisbridges.com>
2017-07-26 17:10:59 -0400

    remove source dir - invoke from source dir

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
 files="environment bash_profile bashrc zshrc inputrc aliases functions gitconfig gitconfig-riverbed gitignore gitignore_global gitattributes dictionary plan vimrc"
-dir=~/Developer/DotFiles
 
 # Make symlinks
 for file in $files; do
-    # echo "Moving any existing dotfiles from ~ to $olddir"
-    # mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -sf $dir/$file ~/.$file
+    ln -sf $file ~/.$file
 done
+
