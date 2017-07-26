bf025eca34436f4e00fda81dbc2041f812aad307
Curtis Bridges <curtis@curtisbridges.com>
2017-07-26 18:50:06 -0400

    fix linking

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 files="environment bash_profile bashrc zshrc inputrc aliases functions gitconfig gitconfig-riverbed gitignore gitignore_global gitattributes dictionary plan vimrc"
+dir=`pwd`
 
 # Make symlinks
 for file in $files; do
-    echo "Creating symlink to $file in home directory."
-    ln -sf $file ~/.$file
+    echo "Creating symlink to .$file in home directory."
+    ln -sf $dir/$file ~/.$file
 done
 
