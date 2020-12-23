2f9ea4a60037dae1ab413cf8f28b28e3d0c3a2c4
Curtis Bridges <curtis@curtisbridges.com>
2020-12-23 09:10:40 -0500

    handle linux autojump location

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -1,5 +1,6 @@
 # Setup autojump
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
 
 # Install Starship
 starship init fish | source
