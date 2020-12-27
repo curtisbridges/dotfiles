80f7584662efda551184a36d8e20a3ab53041d73
Curtis Bridges <curtis@curtisbridges.com>
2020-12-27 11:04:51 -0500

    add tmux abbr and ripgrep

--- shell/.config/fish/conf.d/abbr.fish
+++ shell/.config/fish/conf.d/abbr.fish
@@ -9,3 +9,12 @@ abbr -a nf 'neofetch'
 abbr -a cat 'bat'
 abbr -a top 'htop'
 abbr -a vim 'nvim'
+abbr -a grep 'ripgrep'
+
+# tmux
+abbr -a tm 'tmux'
+abbr -a tmas 'tmux attach-session -t'
+abbr -a tmls 'tmux list-sessions'
+abbr -a tmks 'tmux kill-session -t'
+abbr -a tmns 'tmux new-session -t'
+
