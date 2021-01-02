0024d11a14ade17bd7cece3f95f8616001fb540a
Curtis Bridges <curtis@curtisbridges.com>
2021-01-02 12:32:16 -0500

    convert from omf to fisher

--- shell/.config/fish/conf.d/abbr.fish
+++ shell/.config/fish/conf.d/abbr.fish
@@ -5,16 +5,18 @@
 abbr -a bup 'brew update; and brew upgrade; and brew upgrade --cask --greedy'
 
 # Simple executable abbreviations
-abbr -a nf 'neofetch'
-abbr -a cat 'bat'
-abbr -a top 'htop'
-abbr -a vim 'nvim'
-abbr -a grep 'ripgrep'
+abbr -a nf   'neofetch'
+abbr -a cat  'bat'
+abbr -a top  'htop'
+abbr -a vim  'nvim'
+abbr -a grep 'rg'
 
 # tmux
-abbr -a tm 'tmux'
+abbr -a tm   'tmux'
 abbr -a tmas 'tmux attach-session -t'
 abbr -a tmls 'tmux list-sessions'
 abbr -a tmks 'tmux kill-session -t'
 abbr -a tmns 'tmux new-session -t'
+
+abbr -a tmx  'tmuxinator'
 
