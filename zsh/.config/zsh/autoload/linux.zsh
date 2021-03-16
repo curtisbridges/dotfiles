5f78d5f1279da5769719d8ed35ff172e5ecc3634
Curtis Bridges <curtis@curtisbridges.com>
2021-03-16 17:15:21 -0400

    fix linux nvm path
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- zsh/.config/zsh/autoload/linux.zsh
+++ zsh/.config/zsh/autoload/linux.zsh
@@ -3,10 +3,17 @@ if [[ `uname` == 'Linux' ]]; then
   # Make 'open' work in Linux (I love it on macOS)
   alias open="xdg-open"
 
+  # make homebrew (on linux!) function
+  eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
   if [[ `lsb_release -i -s` == 'Arch' ]]; then
     # source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
   else
     # source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
     alias bat=batcat
+
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
+
   fi
 fi
