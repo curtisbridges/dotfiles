3da454dc5cfe3ce936350f71ad06044e9cfab5cf
Curtis Bridges <curtis@curtisbridges.com>
2021-05-11 07:06:27 -0400

    switch from ls to exa
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- zsh/.config/zsh/autoload/aliases.zsh
+++ zsh/.config/zsh/autoload/aliases.zsh
@@ -10,3 +10,15 @@ alias ping='prettyping --nolegend'
 alias vim=nvim
 alias yt=youtube-dl
 alias yt4=youtube-dl --format mp4
+
+# general use
+alias ls='exa'                                                       # ls
+alias l='exa -lbF --git  --group-directories-first'                  # list, size, type, git
+alias ll='exa -lbGF --git  --group-directories-first'                # long list
+alias llm='exa -lbGd --git --sort=modified'                          # long list, modified date sort
+alias la='exa -lbhHgUmua --git --color-scale'                        # all list
+alias lx='exa -lbhHgUmua@ --time-style=long-iso --git --color-scale' # all + extended list
+
+# specialty views
+alias lS='exa -1'                                                       # one column, just names
+alias lt='exa --tree --level=2'                                         # tree
