25664f93e91f61b6ddc149cc3be047101ae03e75
Curtis Bridges <curtis@curtisbridges.com>
2021-06-20 07:24:44 -0400

    fix homebrew on debian based
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- zsh/.config/zsh/autoload/linux.zsh
+++ zsh/.config/zsh/autoload/linux.zsh
@@ -5,19 +5,20 @@ if [[ `uname` == 'Linux' ]]; then
 
   # make homebrew (on linux!) function
   set -o extendedglob
-  if [[ "~/.linuxbrew/bin/brew" ]] then
-      eval $(~/.linuxbrew/bin/brew shellenv)
-  elif [[ "/home/linuxbrew/.linuxbrew/bin/brew" ]] then
-      eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  if [[ -f "~/.linuxbrew/bin/brew" ]]; then
+    eval $(~/.linuxbrew/bin/brew shellenv)
+    [ -s "~/.linuxbrew/opt/nvm/nvm.sh" ] && . "~/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
+    source ~/.linuxbrew/share/zsh-you-should-use/you-should-use.plugin.zsh
+  elif [[ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
+    eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
+    source /home/linuxbrew/.linuxbrew/share/zsh-you-should-use/you-should-use.plugin.zsh
   fi
 
   # source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-  source ~/.linuxbrew/share/zsh-you-should-use/you-should-use.plugin.zsh
   # alias bat=batcat
 
   # nvm & node setup
   #export NVM_DIR="$HOME/.nvm"
-  source $NVM_DIR/nvm.sh
-  # [ -s "~/.linuxbrew/opt/nvm/nvm.sh" ] && . "~/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
-  # [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
+  # source $NVM_DIR/nvm.sh
 fi
