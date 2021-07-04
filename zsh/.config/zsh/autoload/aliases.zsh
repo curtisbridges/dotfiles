0210e52882d04b6f4c656924f4b8c70a4c4a02e4
Curtis Bridges <curtis@curtisbridges.com>
2021-07-04 13:57:14 -0400

    mostly tmux changes
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- zsh/.config/zsh/autoload/aliases.zsh
+++ zsh/.config/zsh/autoload/aliases.zsh
@@ -11,8 +11,11 @@ alias vim=nvim
 alias yt=youtube-dl
 alias yt4=youtube-dl --format mp4
 
+# post-pended aliases
+alias -g F='| fzf'
+
 # general use
-alias ls='exa -F'                                                       # ls
+alias ls='exa -F'                                                    # ls
 alias l='exa -lbF --git  --group-directories-first'                  # list, size, type, git
 alias ll='exa -lbGF --git  --group-directories-first'                # long list
 alias llm='exa -lbGd --git --sort=modified'                          # long list, modified date sort
@@ -24,5 +27,6 @@ alias lS='exa -1'                                                       # one co
 alias lt='exa --tree --level=2'                                         # tree
 
 # Debug PATH
+# NOTE: This double quote gets evaluated greedily and needs to be replaced with something else.
 alias pp="echo $PATH | sed 's/:/\n/g'"
 alias ppu="echo $PATH | sed 's/:/\n/g' | sort | uniq -c"
