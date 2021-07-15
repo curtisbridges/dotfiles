968480af7919e603f0092621edca1aeb53ab9be7
Curtis Bridges <curtis@curtisbridges.com>
2021-07-15 16:22:02 -0400

    add brew sbin to path
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- zsh/.config/zsh/autoload/mac.zsh
+++ zsh/.config/zsh/autoload/mac.zsh
@@ -9,6 +9,9 @@ if [[ `uname` == 'Darwin' ]]; then
     #     }
     # fi
 
+    # Homebrew sbin
+    export PATH="/usr/local/sbin:$PATH"
+
     # Node setup -- homebrew nvm lives in homebrew installed dirs
     # [ -s "$(brew --prefix)/opt/nvm/nvm.sh" ] && . "$(brew --prefix)/opt/nvm/nvm.sh" # This loads nvm
     # [ -s "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" ] && . "$(brew --prefix)/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
