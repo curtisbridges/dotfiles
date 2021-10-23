9f139cb4a11bec560513370fcc2d9e9ea7b641a6
Curtis Bridges <curtis@curtisbridges.com>
2021-10-23 07:43:26 -0400

    remove nvm from zsh
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- zsh/.config/zsh/autoload/linux.zsh
+++ zsh/.config/zsh/autoload/linux.zsh
@@ -6,22 +6,23 @@ if [[ `uname` == 'Linux' ]]; then
   # Make 'open' work in Linux (I love it on macOS)
   alias open="xdg-open"
 
+  # trash aliases for linux
+  alias trash=trash-put
+  alias trpt=trash-put
+  alias trem=trash-empty
+  alias trls=trash-list
+  alias trrs=trash-restore
+  alias trrm=trash-rm
+
   # make homebrew (on linux!) function
   set -o extendedglob
   if [[ -f "~/.linuxbrew/bin/brew" ]]; then
     eval $(~/.linuxbrew/bin/brew shellenv)
-    [ -s "~/.linuxbrew/opt/nvm/nvm.sh" ] && . "~/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
     source ~/.linuxbrew/share/zsh-you-should-use/you-should-use.plugin.zsh
   elif [[ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
     eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-    [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && . "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
     source /home/linuxbrew/.linuxbrew/share/zsh-you-should-use/you-should-use.plugin.zsh
   fi
-
   # source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
   # alias bat=batcat
-
-  # nvm & node setup
-  #export NVM_DIR="$HOME/.nvm"
-  # source $NVM_DIR/nvm.sh
 fi
