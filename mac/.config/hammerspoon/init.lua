b057769d6ab49a128f1805918541965abd06a4ed
Curtis Bridges <curtis@curtisbridges.com>
2023-03-18 09:24:40 -0400

    big hammerspoon change - simplicity
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- mac/.config/hammerspoon/init.lua
+++ mac/.config/hammerspoon/init.lua
@@ -6,39 +6,13 @@
 meh = {'⌘', '⌥', '⌃'} -- bottom row modifiers
 hyper = {'⌘', '⌥', '⇧', '⌃'} -- caps lock held down (karabiner hyper)
 
--- Spoons!
--- Download and install this _one_ spoon to autoload all the others
-hs.loadSpoon('SpoonInstall')
-Install = spoon.SpoonInstall
-spoon.SpoonInstall.use_syncinstall = true
-
--- Clipboard manager
--- Install:andUse('ClipboardTool')
--- spoon.ClipboardTool.paste_on_select = true
--- spoon.ClipboardTool.show_in_menubar = false
--- spoon.ClipboardTool.show_copied_alert = false
--- spoon.ClipboardTool:start()
--- hs.hotkey.bind(hyper, 'v', function() spoon.ClipboardTool:toggleClipboard() end)
-
--- Window Management
-Install:andUse('ModalMgr')
-Install:andUse('WinWin')
-
--- Shortcut overlay
-Install:andUse('KSheet', { hotkeys = { toggle = {meh, '/'}}})
-
 -- my modules
+require 'quake'
 require 'utils'
-require 'app-switcher'
-require 'window-custom'
-require 'window-modal'
-
--- Finally we initialize ModalMgr supervisor
-spoon.ModalMgr.supervisor:enter()
 
 -- hammerspoon
-hs.hotkey.bind(hyper, 'delete', function() hs.reload() end)
-hs.hotkey.bind(meh, '/', function() hs.hotkey.showHotkeys() end)
+-- hs.hotkey.bind(hyper, 'delete', function() hs.reload() end)
+-- hs.hotkey.bind(meh, '/', function() hs.hotkey.showHotkeys() end)
 
 -- show Hammerspoon loaded
 hs.alert('Hammerspoon loaded')
---
+++ mac/.config/hammerspoon/quake.lua
@@ -0,0 +1,8 @@
+hs.hotkey.bind({ "shift", "command" }, "return", function()
+  local alacritty = hs.application.find('alacritty')
+  if alacritty:isFrontmost() then
+    alacritty:hide()
+  else
+    hs.application.launchOrFocus("/Applications/Alacritty.app")
+  end
+end)
