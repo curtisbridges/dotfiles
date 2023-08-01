76d493289147b47f3207ea78465142988e84b6e2
Curtis Bridges <curtis@curtisbridges.com>
2023-08-01 08:49:38 -0400

    new launch scrips
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- zsh/.config/zsh/autoload/skillsoft.zsh
+++ zsh/.config/zsh/autoload/skillsoft.zsh
@@ -1,3 +1,8 @@
 # Work AWS
 alias ecrlogin="aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 510467250861.dkr.ecr.us-east-1.amazonaws.com"
 export AWS_PROFILE=dre
+
+# front build script
+alias do='node ./scripts/do'
+
+export USER_TERMINAL=Alacritty
