d3304a1cfa4efe8778152004f3b03c3bba284d99
Curtis Bridges <curtis@curtisbridges.com>
2023-08-25 13:18:35 -0400

    fish cleanup
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- fish/.config/fish/functions/mkcd.fish
+++
@@ -1,4 +0,0 @@
-function mkcd --description 'Create a directory and change to it immediately'
-  mkdir $argv
-  cd $argv
-end
--- fish/.config/fish/functions/update.fish
+++
@@ -1,9 +0,0 @@
-function update --description 'Get OS X Software Updates and update Homebrew, mas, and npm packages'
-  sudo softwareupdate -i -a
-  brew update
-  brew upgrade --force
-  brew cleanup
-  mas upgrade
-  npm install npm -g
-  npm update -g
-end
