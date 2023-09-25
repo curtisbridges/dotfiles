156da027b7f9b1f8fff043f6eca484cd0a9347fc
Curtis Bridges <curtis@curtisbridges.com>
2023-09-25 06:33:44 -0400

    playing with rectangle-pro

--- hammerspoon/.config/hammerspoon/config.lua
+++ hammerspoon/.config/hammerspoon/config.lua
@@ -8,16 +8,17 @@ mod_focus = { '⌘', '⌃' }
 mod_move = { '⌥', '⌃' }
 mod_resize = { '⌥', '⌘' }
 
+-- Monitors
 MACBOOK_MONITOR = 'Built-in Retina Display'
 MAIN_MONITOR = 'DELL U3219Q'
 SECOND_MONITOR = 'DELL U2723QE (1)'
 THIRD_MONITOR = 'DELL U2723QE (2)'
 
+-- Apps
+TERMINAL_APP = 'Alacritty'
+
 -- faster animations, default value = 0.2
 hs.window.animationDuration = .1
 
 -- Binds
-hs.hotkey.bind(meh, "space", function() quakeTerminal() end)
-
--- Apps
-TERMINAL_APP = 'Alacritty'
+hs.hotkey.bind(hyper, "space", function() quakeTerminal() end)
