5776cdcd1da91075804f59a7be3c908958e1205e
Curtis Bridges <curtis@curtisbridges.com>
2023-10-29 08:40:08 -0400

    fix exports related to fzf searching
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- zsh/.config/zsh/before/exports.zsh
+++ zsh/.config/zsh/before/exports.zsh
@@ -4,12 +4,14 @@
 export FZF_BASE=$(brew --prefix)/bin/fzf
 
 # Make fzf smarter
-export FZF_DEFAULT_COMMAND='rg --files --follow --no-ignore-vcs --hidden -g "!{node_modules/*,.git/*,.DS_STORE}"'
+# export FZF_DEFAULT_COMMAND='rg --files --follow --no-ignore-vcs --hidden -g "!{node_modules/*,.git/*,.DS_STORE}"'
+export FZF_DEFAULT_COMMAND='rg --files --follow --hidden -g "!{node_modules/*,.git/*,.DS_STORE}"'
 export FZF_DEFAULT_OPTS='--height 50% --layout=reverse --border --ansi'
 
 # export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 # export FZF_CTRL_T_COMMAND="fd --type f --hidden --follow --exclude '{....}'"
-export FZF_CTRL_T_COMMAND='rg --files --follow --no-ignore-vcs --hidden -g "!{node_modules/*,.git/*,.DS_STORE}"'
+# export FZF_CTRL_T_COMMAND='rg --files --follow --no-ignore-vcs --hidden -g "!{node_modules/*,.git/*,.DS_STORE}"'
+export FZF_CTRL_T_COMMAND='rg --files --follow --hidden -g "!{node_modules/*,.git/*,.DS_STORE}"'
 
 # Node related
 export PYTHON_HOME="${HOME}/.pyenv/shims"
