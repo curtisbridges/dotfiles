76a59b15db26fd90630678a51d3ac9012308622b
Curtis Bridges <curtis@curtisbridges.com>
2024-02-15 12:51:26 -0500

    back to basics starship
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- zsh/.config/zsh/before/exports.zsh
+++ zsh/.config/zsh/before/exports.zsh
@@ -1,7 +1,8 @@
 # Exported variables
 
 # Node related
-export PYTHON_HOME="${HOME}/.pyenv/shims"
-export PATH=$PATH:$PYTHON_HOME
 export NVM_DIR="$HOME/.nvm"
 
+# Python
+export PYTHON_HOME="${HOME}/.pyenv/shims"
+export PATH=$PATH:$PYTHON_HOME
