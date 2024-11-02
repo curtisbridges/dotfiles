02a540824bf31c64f03141a87327744c9f50c213
Curtis Bridges <curtis@curtisbridges.com>
2024-11-02 09:20:19 -0400

    wezterm config updates
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,9 +4,18 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
--- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Macchiato'
+-- Helper function:
+-- returns color scheme dependant on operating system theme setting (dark/light)
+local function color_scheme_for_appearance(appearance)
+  if appearance:find "Dark" then
+    -- return "Tokyo Night"
+    return "Catppuccin Mocha"
+  else
+    return "Tokyo Night Day"
+  end
+end
+
+-- Fonts
 config.font = wezterm.font_with_fallback {
   'SF Mono',
   'Fira Code Nerd Font',
@@ -14,18 +23,21 @@ config.font = wezterm.font_with_fallback {
 }
 config.font_size = 14
 
+-- Appearance
+config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
 config.window_decorations = 'RESIZE'
-config.window_background_opacity = 0.9
-config.macos_window_background_blur = 30
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 10
 config.window_padding = {
   left = 0,
   right = 0,
   top = 0,
   bottom = 0,
 }
-
 config.hide_tab_bar_if_only_one_tab = true
+config.native_macos_fullscreen_mode = false
 
+-- Misc
 config.check_for_updates = false
 
 -- and finally, return the configuration to wezterm
