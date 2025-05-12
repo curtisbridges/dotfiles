61b98b54216095d4c4cc0d2011ef818494728cf9
Curtis Bridges <curtis@curtisbridges.com>
2025-05-12 11:04:03 -0400

    update skillsoft postgres aliases
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- zsh/.config/zsh/skillsoft.zsh
+++ zsh/.config/zsh/skillsoft.zsh
@@ -4,5 +4,8 @@ export AWS_PROFILE=psl
 
 alias rmowner="git checkout -- .github/CODEOWNERS ownership/codeowners.yaml"
 
-alias pg_start="brew services start postgresql@14"
-alias pg_stop="brew services stop postgresql@14"
+alias pg_start="brew services start postgresql@16"
+alias pg_stop="brew services stop postgresql@16"
+
+export PATH="/opt/homebrew/opt/postgresql@16/bin:$PATH"
+
