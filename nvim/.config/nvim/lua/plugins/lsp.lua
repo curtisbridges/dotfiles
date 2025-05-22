e30b408550427f9d8bc327b5fa858110cde94b78
Curtis Bridges <curtis@curtisbridges.com>
2025-05-22 16:19:49 -0400

    switch from tsserver to ts_ls
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,7 +5,7 @@ return {
     local lspconfig = require("lspconfig")
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    lspconfig.tsserver.setup({ capabilities = capabilities })
+    lspconfig.ts_ls.setup({ capabilities = capabilities })
     lspconfig.html.setup({ capabilities = capabilities })
     lspconfig.cssls.setup({ capabilities = capabilities })
     lspconfig.eslint.setup({ capabilities = capabilities })
