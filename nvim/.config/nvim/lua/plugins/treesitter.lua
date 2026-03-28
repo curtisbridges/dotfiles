9bc8e1084115c918406a2ba17676587d6ee6191a
Curtis Bridges <curtis@curtisbridges.com>
2026-03-28 07:55:13 -0400

    fix broke tree-sitter config
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,14 @@
 -- ~/.config/nvim/lua/plugins/treesitter.lua
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  build = ":TSUpdate",
+  'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
+  build = ':TSUpdate',
   config = function()
-    require("nvim-treesitter.configs").setup({ ensure_installed = { "javascript", "typescript" }, highlight = { enable = true } })
+    require('nvim-treesitter').setup({
+      ensure_installed = { 'lua', 'vim', 'javascript', 'typescript' },
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
   end,
 }
+
